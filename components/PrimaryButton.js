4f08032ed40505c8035829626810fb98aa11278a
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-26 13:09:45 +0300

    "Removed StatusBar import and component, updated Text content, and reformatted styles"

---
+++ components/PrimaryButton.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+function PrimaryButton() {}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, TextInput, View } from "react-native";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput placeholder="Enter your guess" />
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({});
